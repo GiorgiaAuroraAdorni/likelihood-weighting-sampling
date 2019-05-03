3be7a3aa01c238c5a2c7784e683cc361c9c3bd50
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-05-03 15:59:17 +0200

    Added as comment the pseudocode of the two functions to compute Likelihood and Weighting

--- sampling.py
+++ sampling.py
@@ -21,3 +21,42 @@ probJtrue_HWA = [# H  W  A  Jtrue
                 ]
 
 bn = [probWtrue, probHtrue, probAtrue_W, probJtrue_HWA]
+# def weighted_sample(bn, e):
+#
+#     w = 1
+#     x
+#
+#     for var in X:
+#         if var #(is an evidence variable with value x in e)
+#             w = w * #P(var = x | parents(var))
+#         else
+#             x = #random sample from P(var | parents(var))
+#
+#
+#
+#     return x, w
+#
+#
+# def likelihood_weighting(X, e, bn, N):
+#     """
+#     :param X: query variable
+#     :param e: observed values fro variables E
+#     :param bn: a bayesian network specifying joint distribution P(X1, ..., Xn)
+#     :param N: the total number of samples to be generated
+#     :return: an estimate of P(X | e)
+#
+#     Local variables:
+#     - weights: a vector of weighted count fro eac value of X, initialy zero
+#     """
+#
+#     weights = list()
+#
+#     for i in range (1, N):
+#         x, w = weighted_sample(bn, e)
+#         weights[x] = weights[x] + w
+#         # x is the value of X in x
+#
+#
+#     # normalized = normalize(weights)
+#
+#     return normalized
